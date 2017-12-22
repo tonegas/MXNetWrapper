f85826f3a2feef5dae291d2182ef4e7f079fd5f3
tonegas <tonegas@gmail.com>
2017-12-23 00:39:12 +0100

    fix of the export of Mathematica for MXNet

--- src/main.cpp
+++ src/main.cpp
@@ -27,29 +27,30 @@ int main(int argc, char* argv[]){
   net.fordward(vett_float);
 
 //------------------------------------------------------------------------------
-//----------------------------Example with my Graph-----------------------------
+//-----------------------Example with my Graph Wrong Export---------------------
 //------------------------------------------------------------------------------
 
-  //
-  // W is a matrix 2 row 3 column
-  // b is a vector of 2
-  // input is a vetor of 3
-  // output is a vetor of 2
-  // output = W * input + b
-  //
-  // Are these inputs right?
   //----------------------------------------------------------
     const mx_uint input_shape_indptr2[] = { 0, 2 };
-    const mx_uint input_shape_data2[] = { 1, 3 };
+    const mx_uint input_shape_data2[] = { 1, 2 };
     const char* input_key2[1];
     input_key2[0] = {"Input"};
   //----------------------------------------------------------
 
-    net_classes::MXNetWrapper net2 = net_classes::MXNetWrapper((std::string)"../networks/netTest", input_key2, input_shape_indptr2, input_shape_data2);
-    std::vector<mx_float> vett_float2 = {1.2,1.0,2.2};
+    net_classes::MXNetWrapper net2 = net_classes::MXNetWrapper((std::string)"../networks/MathematicaNet_Wrong", input_key2, input_shape_indptr2, input_shape_data2);
+    std::vector<mx_float> vett_float2 = {1.2,1.0};
     net2.fordward(vett_float2);
 
-    net_classes::MXNetWrapper net3 = net_classes::MXNetWrapper((std::string)"../networks/netTest", input_key2, input_shape_indptr2, input_shape_data2);
+    net_classes::MXNetWrapper net3 = net_classes::MXNetWrapper((std::string)"../networks/MathematicaNet_Wrong", input_key2, input_shape_indptr2, input_shape_data2);
     net3.fordward(vett_float2);
 
+//------------------------------------------------------------------------------
+//-----------------------Example with my Graph Right Export---------------------
+//------------------------------------------------------------------------------
+
+    net_classes::MXNetWrapper net4 = net_classes::MXNetWrapper((std::string)"../networks/MathematicaNet", input_key2, input_shape_indptr2, input_shape_data2);
+    net4.fordward(vett_float2);
+
+    net_classes::MXNetWrapper net5 = net_classes::MXNetWrapper((std::string)"../networks/MathematicaNet", input_key2, input_shape_indptr2, input_shape_data2);
+    net5.fordward(vett_float2);
 }
