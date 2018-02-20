eb580308ea51a9b810a4742ec2bf59b15360892b
tonegas <tonegas@gmail.com>
2018-02-20 17:44:02 +0100

    Modified main removing Inception

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,25 @@ int main(int argc, char* argv[]){
 //------------------------------------------------------------------------------
 //-------------------------Example with Inception Network-----------------------
 //------------------------------------------------------------------------------
-  int width = 224;
-  int height = 224;
-  int channels = 3;
-  int dim = width*height*channels;
-  const mx_uint input_shape_indptr[] = { 0, 4 };
-  const mx_uint input_shape_data[] = { 1,
-                                        static_cast<mx_uint>(channels),
-                                        static_cast<mx_uint>(height),
-                                        static_cast<mx_uint>(width)};
-  const char* input_key[1];
-  input_key[0] = {"data"};
-
-  std::vector<mx_float> vett_float = std::vector<mx_float>(dim);
-  for (int i = 0 ; i < dim ; i++){
-      vett_float[i] = 0.0;
-  }
-
-  net_classes::MXNetWrapper net = net_classes::MXNetWrapper((std::string)"../networks/Inception-BN",  input_key, input_shape_indptr, input_shape_data);
-  net.fordward(vett_float);
+// int width = 224;
+// int height = 224;
+// int channels = 3;
+// int dim = width*height*channels;
+// const mx_uint input_shape_indptr[] = { 0, 4 };
+// const mx_uint input_shape_data[] = { 1,
+//                                       static_cast<mx_uint>(channels),
+//                                       static_cast<mx_uint>(height),
+//                                       static_cast<mx_uint>(width)};
+// const char* input_key[1];
+// input_key[0] = {"data"};
+//
+// std::vector<mx_float> vett_float = std::vector<mx_float>(dim);
+// for (int i = 0 ; i < dim ; i++){
+//     vett_float[i] = 0.0;
+// }
+//
+// net_classes::MXNetWrapper net = net_classes::MXNetWrapper((std::string)"../networks/Inception-BN",  input_key, input_shape_indptr, input_shape_data);
+// net.fordward(vett_float);
 
 //------------------------------------------------------------------------------
 //-----------------------Example with my Graph Wrong Export---------------------
